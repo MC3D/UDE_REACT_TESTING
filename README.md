2a9cff3d613790da510d08feed089153d52b7492
MC3D <m.chapman.home@gmail.com>
2016-12-29 13:32:56 -0500

    refactors app_test

---
+++ README.md
@@ -0,0 +1,13 @@
+## describe
+
+group together similar tests
+'describe' is used to convey to other developers that a certain number of tests are related in some fashion
+
+## it
+
+to test a single attribute of a target
+'it' blocks try to make an assertion about a very particular fact about the testing subject
+
+## expect
+
+to make a single assertion about a target
--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,14 @@
-import { renderComponent , expect } from '../test_helper';
+import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
-describe('App' , () => {
-  let component;
+// use 'describe' to group together similar tests
+describe('App', () => {
 
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
-
-  it('renders something', () => {
-    expect(component).to.exist;
+  // use 'it' to test a single attribute of a target
+  it('should show the correct text', () => {
+    // create an instance of App
+    const component = renderComponent(App);
+    // use 'expect' to make an 'assertion' about a target; expect is an function that returns an object
+    expect(component).to.contain('React simple starter');
   });
 });
