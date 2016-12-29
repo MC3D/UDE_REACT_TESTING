d7e77db48ab9439ef3f6ebddb7a682c038b7c6a2
MC3D <m.chapman.home@gmail.com>
2016-12-29 15:05:20 -0500

    refactors comment_box_test

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
 
 export default class CommentBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { comment: '' };
+  }
+  _handleInput(event) {
+    this.setState({ comment: event.target.value });
+  }
+
   render() {
     return (
       <div className='comment-box'>
-        <textarea />
+        <textarea value={this.state.comment} onChange={this._handleInput.bind(this)} />
         <button>Submit Comment</button>
       </div>
     )
--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -8,15 +8,33 @@ describe('CommentBox', () => {
     component = renderComponent(CommentBox);
   });
 
-  it('has the correct class', () =>{
-    expect(component).to.have.class('comment-box');
+  describe('component layout', () => {
+    it('has the correct class', () =>{
+      expect(component).to.have.class('comment-box');
+    });
+
+    it('has a text area', () => {
+      expect(component.find('textarea')).to.exist;
+    });
+
+    it('has a button', () => {
+      expect(component.find('button')).to.exist;
+    });
   });
 
-  it('has a text area', () => {
-    expect(component.find('textarea')).to.exist;
+  describe('component behavior', () => {
+    beforeEach(() => {
+      component.find('textarea').simulate('change', 'new comment');
+    });
+
+    it('shows user input', () => {
+      expect(component.find('textarea')).to.have.value('new comment')
+    });
+
+    it('clears input on submit', () => {
+
+    });
   });
-  
-  it('has a button', () => {
-    expect(component.find('button')).to.exist;
-  });
+
+
 });
