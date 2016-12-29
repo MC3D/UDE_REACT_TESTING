b71d559dbc56ac1928ee01351d809f619f353c42
MC3D <m.chapman.home@gmail.com>
2016-12-29 14:27:17 -0500

    adds comment_box_test

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -5,7 +5,7 @@ import App from '../../src/components/app';
 describe('App', () => {
 
   // use 'it' to test a single attribute of a target
-  it('should show the correct text', () => {
+  it('shows the correct text', () => {
     // create an instance of App
     const component = renderComponent(App);
     // use 'expect' to make an 'assertion' about a target; expect is an function that returns an object
