643bfe4142ced6ed2b0ac280e802a37a9983c404
MC3D <m.chapman.home@gmail.com>
2016-12-29 14:48:29 -0500

    refactors app

--- test/components/comment_box_test.js
+++ test/components/comment_box_test.js
@@ -11,9 +11,11 @@ describe('CommentBox', () => {
   it('has the correct class', () =>{
     expect(component).to.have.class('comment-box');
   });
+
   it('has a text area', () => {
     expect(component.find('textarea')).to.exist;
   });
+  
   it('has a button', () => {
     expect(component.find('button')).to.exist;
   });
